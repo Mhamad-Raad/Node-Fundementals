bab850594b266659872f4b06d05bda3abfbe2923
joker69216 <hamaraad883@gmail.com>
2023-05-22 19:25:12 +0300

    make the responses more dynamic

--- index.js
+++ index.js
@@ -3,13 +3,45 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
+  // res.writeHead(200, {
+  //   'Content-Type': 'application/json',
+  // });
 
-  res.end(JSON.stringify({
-    message: 'Hello World!!!',
-  }));
+  if (req.url === '/friends') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+      JSON.stringify([
+        {
+          name: 'Ali',
+          id: 1,
+        },
+        {
+          name: 'Ahmed',
+          id: 2,
+        },
+      ])
+    );
+  } else if (req.url === '/message') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(
+      JSON.stringify([
+        {
+          text: 'Hello World!',
+          id: 1,
+        },
+        {
+          text: 'Hello World!!',
+          id: 2,
+        },
+      ])
+    );
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
